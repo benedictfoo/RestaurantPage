77569a0d95d205040219647ce192f5fc7083e46b
benedictfoo <200115050+benedictfoo@users.noreply.github.com>
2025-04-03 14:58:56 +0800

    Add about with styling

---
+++ src/js/about.js
@@ -0,0 +1,17 @@
+const contentElement = document.querySelector("#content");
+contentElement.innerHTML = "";
+const heading = document.createElement("h1");
+heading.textContent = "About Us";
+contentElement.appendChild(heading);
+const paragraphs = [
+  "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Veniam asperiores beatae accusantium ducimus suscipit tenetur aliquam quis aut corrupti debitis!",
+  " Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem eligendi, temporibus mollitia repellendus non excepturi? Error incidunt hic corporis. Quidem eaque laboriosam sint! Fugit odio voluptas incidunt. In, doloremque cum?",
+];
+paragraphs.forEach((paragraph) => {
+  const paragraphElement = document.createElement("p");
+  paragraphElement.style.lineHeight = 2;
+  paragraphElement.style.marginBottom = "2rem";
+
+  paragraphElement.textContent = paragraph;
+  contentElement.appendChild(paragraphElement);
+});
